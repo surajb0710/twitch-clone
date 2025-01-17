d6e91dc8be1cb6ba355e7e2d02aaa7f05f7d51cd
Suraj <104007414+surajb0710@users.noreply.github.com>
2025-01-17 12:56:42 +0530

    Empty User Avatar added

--- app/(browse)/_components/sidebar/index.tsx
+++ app/(browse)/_components/sidebar/index.tsx
@@ -1,10 +1,17 @@
 import Wrapper from './wrapper';
 import Toggle from './toggle';
+import Recommended from './recommended';
+import { getRecommended } from '@/lib/recommended-service';
 
-const Sidebar = () => {
+const Sidebar = async () => {
+  const recommended = await getRecommended();
+
   return (
     <Wrapper>
       <Toggle />
+      <div className="space-y-4 pt-4 lg:pt-0">
+        <Recommended data={recommended} />
+      </div>
     </Wrapper>
   );
 };
---
+++ components/user-avatar.tsx
@@ -0,0 +1,8 @@
+import { cn } from '@/lib/utils';
+import { cva, type VariantProps } from 'class-variance-authority';
+import { Skeleton } from './ui/skeleton';
+import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
+
+export const UserAvatar = () => {
+  return <div>UserAvatar</div>;
+};
